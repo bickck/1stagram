2f0bb81157d9dcc77f61fec9a5dabc9ded602e72
USER <USER@192.168.219.100>
2022-06-08 22:00:24 +0900

    register

--- src/main/java/com/team/Imitation/repository/dto/MemberDTO.java
+++ src/main/java/com/team/Imitation/repository/dto/MemberDTO.java
@@ -1,6 +1,57 @@
 package com.team.Imitation.repository.dto;
 
 public class MemberDTO {
-
-	private 
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String birth;
+	private String address;
+	private String create_at;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCreate_at() {
+		return create_at;
+	}
+	public void setCreate_at(String create_at) {
+		this.create_at = create_at;
+	}
+	
+	
+	
 }
