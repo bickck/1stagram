c5a8e74023c1c3f64f76c4f4cf0d30e74f7ce40a
Donghyeon Kim <30370732+bickck@users.noreply.github.com>
2022-06-08 15:21:41 +0900

    Delete 1stagram-master directory

--- 1stagram-master/src/main/java/com/team/Imitation/repository/LoginMapper.java
+++
@@ -1,9 +0,0 @@
-package com.team.Imitation.repository;
-
-import com.team.Imitation.repository.dto.LoginDTO;
-
-public interface LoginMapper {
-
-	public void login(LoginDTO dto);
-	
-}
--- 1stagram-master/src/main/java/com/team/Imitation/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.team.Imitation.service;
-
-import com.team.Imitation.repository.dto.MemberDTO;
-
-public interface MemberService {
-
-	public int register(MemberDTO member);
-
-}
